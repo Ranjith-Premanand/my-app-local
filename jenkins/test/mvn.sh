de86ece56893900201deaa7a4ad97f44a23d6f0e
Ranjith Premanand <ranjithpremanand7@gmail.com>
2020-10-29 10:15:33 +0000

    new files

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "*****Building JAR*****"
-docker run -ti -v /home/vagrant/jenkins-data/jenkins_home/workspace/CI-CD:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+echo "*****Testing JAR*****"
+docker run --rm -v /var/jenkins_home/workspace/CI-CD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
