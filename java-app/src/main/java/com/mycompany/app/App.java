1ec434a0f419fd657f70edc57fc4d9e1b8d0eeb2
Ranjith Premanand <ranjithpremanand7@gmail.com>
2020-10-30 18:31:27 +0000

    Final code

--- java-app/src/main/java/com/mycompany/app/App.java
+++ java-app/src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello from Devops!";
+    private final String message = "Finally CI/CD pipeline achieved!";
 
     public App() {}
 
