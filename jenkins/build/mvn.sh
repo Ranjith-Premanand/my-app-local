d18cca68168969773663767dd7a001a8c831f9af
Ranjith Premanand <65222380+Ranjith-Premanand@users.noreply.github.com>
2020-11-02 20:18:10 +0530

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "*****Building JAR*****"
-docker run --rm -v /home/vagrant/jenkins-data/jenkins_home/workspace/CI-CD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -v /home/ubuntu/jenkins-data/jenkins_home/workspace/CI-CD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
