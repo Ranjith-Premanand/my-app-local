7291e456abb092724e3cf1922a9bde185ff0e496
Ranjith <ranjithperks@gmail.com>
2020-11-09 14:07:13 +0530

    new files

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "*****Testing JAR*****"
-docker run --rm -v /home/vagrant/jenkins-data/jenkins_home/workspace/CI-CD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -v C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\pipeline-job\java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
